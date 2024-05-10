ba1c7a3b1ddf5cd6731ab928b049d0ae1807102e
Greg Brown <gregbrowndev@gmail.com>
2024-05-10 12:38:37 +0100

    wip: URQL init now seems reliable
    I was seeing an error in the browser console that looked like it was including code from @/lib/server, only the string was truncated, e.g. something like require("__webpack....
    
    I've now converted the GraphqlProvider into essentially
    a pointer to the component rather than wrapping the components again. This seems to have fixed the issue

--- apps/web-ui/src/contexts/graphql/provider.tsx
+++ apps/web-ui/src/contexts/graphql/provider.tsx
@@ -1,17 +1,7 @@
 "use client";
-
-import React from "react";
 import {GraphqlClientProvider} from "@/contexts/graphql/provider-client.tsx";
 import {GraphqlServerProvider} from "@/contexts/graphql/provider-server.tsx";
 
-
-export function GraphqlProvider({ children }: React.PropsWithChildren) {
-    const isClient = typeof window !== "undefined";
-    const Provider = isClient ? GraphqlClientProvider : GraphqlServerProvider;
-
-    return (
-        <Provider>
-            {children}
-        </Provider>
-    );
-}
+export const GraphqlProvider = typeof window !== "undefined"
+    ? GraphqlClientProvider
+    : GraphqlServerProvider;
